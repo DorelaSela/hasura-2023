fd95411c99ad7de1f6818c9c3599577a9d70cc9f
FjonaMalo <95534218+FjonaMalo@users.noreply.github.com>
2023-07-12 12:24:13 +0000

    Delete relations and a specific engineer

--- react-badges/src/components/Backoffice/Engineers/EngineerData/Engineers.jsx
+++ react-badges/src/components/Backoffice/Engineers/EngineerData/Engineers.jsx
@@ -3,6 +3,7 @@ import { Button, TextField } from "@mui/material";
 import { useQuery, gql } from "@apollo/client";
 import { LOAD_ENGINEERS } from "../GraphQl/Queries";
 import { CREATE_ENGINEERS_MUTATION } from "../GraphQl/Mutations";
+import { DELETE_ENGINEERS } from "../GraphQl/Mutations";
 import { useMutation } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 
@@ -11,6 +12,7 @@ const Engineers = () => {
   const [engineers, setEngineers] = useState([]);
   const [name, setName] = useState("");
   const [insert_engineers, { error }] = useMutation(CREATE_ENGINEERS_MUTATION);
+  const [deleteRelationEngineers] = useMutation(DELETE_ENGINEERS);
   // const navigate = useNavigate();
 
   if (error) {
@@ -33,10 +35,23 @@ const Engineers = () => {
     setName("");
   };
 
+  const deleteEngineers = (id) => {
+    deleteRelationEngineers({
+      variables: {
+        id
+      }
+    });
+  };
+
   return (
     <div>
       {engineers.map((engineer, index) => {
-        return <h3 key={index}>{engineer.name}</h3>;
+        return (
+          <div key={index}>
+            <h3>{engineer.name}</h3>
+            <Button onClick={() => deleteEngineers(engineer.id)}>Delete</Button>
+          </div>
+        );
       })}
       <button onClick={addEngineer}>Create New</button>
       <form
--- react-badges/src/components/Backoffice/Engineers/GraphQl/Mutations.js
+++ react-badges/src/components/Backoffice/Engineers/GraphQl/Mutations.js
@@ -2,9 +2,27 @@ import { gql } from "@apollo/client";
 
 export const CREATE_ENGINEERS_MUTATION = gql`
   mutation CreateEngineer($name: String!) {
-    insert_users_one(object: { name: $name, roles:["engineer"] }) {
+    insert_users_one(object: { name: $name, roles: ["engineer"] }) {
       id
       name
+    }
+  }
+`;
+
+export const DELETE_ENGINEERS = gql`
+  mutation DeleteEngineers($id: Int!) {
+    deleteRelationManager: delete_users_relations(
+      where: { manager: { _eq: $id } }
+    ) {
+      affected_rows
+    }
+    deleteRelationEngineer: delete_users_relations(
+      where: { engineer: { _eq: $id } }
+    ) {
+      affected_rows
+    }
+    delete_users(where: { id: { _eq: $id } }) {
+      affected_rows
     }
   }
 `;
